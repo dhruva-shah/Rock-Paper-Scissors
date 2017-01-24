58ce83fde53119aa38b05e9e9f10e3a2659e1a40
dhruva-shah <shahdhruva811@gmail.com>
2017-01-24 12:22:55 -0500

    initial commit

---
+++ Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Player:<br />
+        <br />
+        Choose :&nbsp;
+        <asp:RadioButton ID="rb_rock" runat="server" Text="Rock" checked="true" Groupname="user"/>
+&nbsp;<asp:RadioButton ID="rb_paper" runat="server" Text="Paper"  Groupname="user"/>
+&nbsp;<asp:RadioButton ID="rb_scissor" runat="server" Text="Scissor" Groupname="user" />
+        <br />
+        <br />
+        <br />
+        Computer :&nbsp;&nbsp; 
+        <asp:Label ID="PC_Rock" runat="server" BorderColor="Black" BorderStyle="Outset" BorderWidth="1px" Text="Rock"></asp:Label>
+&nbsp;&nbsp;
+        <asp:Label ID="PC_Paper" runat="server" BorderColor="Black" BorderStyle="Outset" BorderWidth="1px" Text="Paper"></asp:Label>
+&nbsp;&nbsp;
+        <asp:Label ID="PC_Scissor" runat="server" BorderColor="Black" BorderStyle="Outset" BorderWidth="1px" Text="Scissor"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label_result" runat="server" Text="Label" Visible="False"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button_play" runat="server" Text="Play" OnClick="Button_play_Click" />
+    
+    &nbsp;<asp:Button ID="Button_replay" runat="server" OnClick="Button_replay_Click" Text="Replay" Visible="False" />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public int selected, randomNumber;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+
+
+    protected void Button_play_Click(object sender, EventArgs e)
+    {
+        if (rb_scissor.Checked == true) { selected = 3; }
+        else if (rb_paper.Checked == true) { selected = 2; }
+        else if(rb_rock.Checked == true) { selected = 1; }
+
+        generate();
+        result(selected, randomNumber);
+        Button_play.Visible = false;
+        Button_replay.Visible = true;
+        
+    }
+    public void generate()
+    {
+        Random ran = new Random();
+        randomNumber = ran.Next(1, 4);
+
+        if (randomNumber == 3)
+        {
+            PC_Scissor.BackColor = System.Drawing.Color.BlanchedAlmond;
+        }
+        else if (randomNumber == 2)
+        {
+            PC_Paper.BackColor = System.Drawing.Color.BlanchedAlmond;
+        }
+        else if (randomNumber == 1)
+        {
+            PC_Rock.BackColor = System.Drawing.Color.BlanchedAlmond;
+        }
+
+    }
+
+    public void result(int user, int gen)
+    {
+        if (user == gen)
+        {
+            Label_result.Text = "It's a tie";
+            Label_result.Visible = true;
+        }
+        else
+        {
+            if (user == 3)
+            {
+                if (gen == 2)
+                {
+                    Label_result.Text = "You win";
+                    Label_result.Visible = true;
+                }
+                else
+                {
+                    Label_result.Text = "You lose";
+                    Label_result.Visible = true;
+                }
+
+            }
+            else if (user == 2)
+            {
+                if (gen==1)
+                {
+                    Label_result.Text = "You win";
+                    Label_result.Visible = true;
+                }
+                else
+                {
+                    Label_result.Text = "You lose";
+                    Label_result.Visible = true;
+                }  
+            }
+            else if (user == 1)
+            {
+                if (gen == 2) {
+                    Label_result.Text = "You lose";
+                    Label_result.Visible = true;
+                }
+                else {
+                    Label_result.Text = "You win";
+                    Label_result.Visible = true;
+                }
+            }
+
+        }
+    }
+
+
+
+
+    protected void Button_replay_Click(object sender, EventArgs e)
+    {
+        Button_replay.Visible = false;
+        Button_play.Visible = true;
+        Label_result.Visible = false;
+        PC_Rock.BackColor = System.Drawing.Color.White;
+        PC_Paper.BackColor = System.Drawing.Color.White;
+        PC_Scissor.BackColor = System.Drawing.Color.White;
+    }
+}
